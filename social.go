4cdeb1e6f3de1888275a7d08094baa0d60444edc
Morgan Creekmore <gamingrobot@gmail.com>
2014-02-27 20:41:43 -0700

    moved friends stuff to friendscache

--- social.go
+++ social.go
@@ -4,18 +4,13 @@ import (
 	"bytes"
 	"code.google.com/p/goprotobuf/proto"
 	"encoding/binary"
+	"github.com/GamingRobot/steamgo/friendcache"
 	. "github.com/GamingRobot/steamgo/internal"
 	. "github.com/GamingRobot/steamgo/steamid"
 	"sync"
 )
 
 // Provides access to social aspects of Steam.
-//
-// Friend and group lists are implemented as doubly-linked lists for thread-safety.
-// They can be iterated over like so:
-// 	for friend := client.Social.Friends.First(); friend != nil; friend = friend.Next() {
-// 		log.Println(friend.SteamId())
-// 	}
 type Social struct {
 	mutex sync.RWMutex
 
@@ -23,16 +18,16 @@ type Social struct {
 	avatarHash   []byte
 	personaState EPersonaState
 
-	Friends *FriendsList
-	Groups  *GroupsList
+	Friends *friendcache.FriendsList
+	Groups  *friendcache.GroupsList
 
 	client *Client
 }
 
 func newSocial(client *Client) *Social {
 	return &Social{
-		Friends: &FriendsList{byId: make(map[SteamId]*Friend)},
-		Groups:  &GroupsList{byId: make(map[SteamId]*Group)},
+		Friends: friendcache.NewFriendsList(),
+		Groups:  friendcache.NewGroupsList(),
 		client:  client,
 	}
 }
@@ -259,11 +254,11 @@ func (s *Social) handleFriendsList(packet *PacketMsg) {
 		if isClan {
 			rel := EClanRelationship(friend.GetEfriendrelationship())
 			if rel == EClanRelationship_None {
-				s.Groups.remove(steamId)
+				s.Groups.Remove(steamId)
 			} else {
-				s.Groups.add(&Group{
-					steamId:      steamId,
-					relationship: rel,
+				s.Groups.Add(&friendcache.Group{
+					SteamId:      steamId,
+					Relationship: rel,
 				})
 			}
 			if list.GetBincremental() {
@@ -272,11 +267,11 @@ func (s *Social) handleFriendsList(packet *PacketMsg) {
 		} else {
 			rel := EFriendRelationship(friend.GetEfriendrelationship())
 			if rel == EFriendRelationship_None {
-				s.Friends.remove(steamId)
+				s.Friends.Remove(steamId)
 			} else {
-				s.Friends.add(&Friend{
-					steamId:      steamId,
-					relationship: rel,
+				s.Friends.Add(&friendcache.Friend{
+					SteamId:      steamId,
+					Relationship: rel,
 				})
 			}
 			if list.GetBincremental() {
@@ -388,256 +383,4 @@ func (s *Social) handleProfileInfoResponse(packet *PacketMsg) {
 	body := new(CMsgClientFriendProfileInfoResponse)
 	packet.ReadProtoMsg(body)
 	//fmt.Printf("%+v\n", body)
-}
-
-// A thread-safe friend list which contains references to its predecessor and successor.
-// It is mutable and will be changed by Social.
-type FriendsList struct {
-	mutex sync.RWMutex
-
-	first *Friend
-	last  *Friend
-
-	byId map[SteamId]*Friend // fast lookup by ID
-}
-
-func (list *FriendsList) add(friend *Friend) {
-	list.mutex.Lock()
-	defer list.mutex.Unlock()
-
-	friend.mutex = &list.mutex
-
-	list.byId[friend.steamId] = friend
-
-	if list.first == nil {
-		list.first = friend
-		list.last = friend
-	} else {
-		friend.prev = list.last
-		list.last.next = friend
-		list.last = friend
-	}
-}
-
-func (list *FriendsList) remove(id SteamId) {
-	list.mutex.Lock()
-	defer list.mutex.Unlock()
-
-	friend := list.byId[id]
-	if friend == nil {
-		return
-	}
-
-	if list.first == friend {
-		list.first = nil
-	} else {
-		friend.prev.next = friend.next
-	}
-
-	if list.last == friend {
-		list.last = nil
-	} else {
-		friend.next.prev = friend.prev
-	}
-}
-
-func (f *FriendsList) First() *Friend {
-	f.mutex.RLock()
-	defer f.mutex.RUnlock()
-	return f.first
-}
-
-func (f *FriendsList) Last() *Friend {
-	f.mutex.RLock()
-	defer f.mutex.RUnlock()
-	return f.last
-}
-
-func (f *FriendsList) ById(id SteamId) *Friend {
-	f.mutex.RLock()
-	defer f.mutex.RUnlock()
-	return f.byId[id]
-}
-
-func (f *FriendsList) Count() int {
-	f.mutex.Lock()
-	defer f.mutex.Unlock()
-	return len(f.byId)
-}
-
-// A thread-safe friend in a friend list which contains references to its predecessor and successor.
-// It is mutable and will be changed by Social.
-type Friend struct {
-	mutex *sync.RWMutex
-
-	prev *Friend
-	next *Friend
-
-	steamId           SteamId
-	name              string
-	relationship      EFriendRelationship
-	personaStateFlags EPersonaStateFlag
-
-	gameAppId uint64
-}
-
-func (f *Friend) Next() *Friend {
-	f.mutex.RLock()
-	defer f.mutex.RUnlock()
-	return f.next
-}
-
-func (f *Friend) Prev() *Friend {
-	f.mutex.RLock()
-	defer f.mutex.RUnlock()
-	return f.prev
-}
-
-func (f *Friend) SteamId() SteamId {
-	// the steam id of a friend never changes, so we don't need to lock here
-	return f.steamId
-}
-
-func (f *Friend) Name() string {
-	f.mutex.RLock()
-	defer f.mutex.RUnlock()
-	return f.name
-}
-
-func (f *Friend) Relationship() EFriendRelationship {
-	f.mutex.RLock()
-	defer f.mutex.RUnlock()
-	return f.relationship
-}
-
-func (f *Friend) PersonaStateFlags() EPersonaStateFlag {
-	f.mutex.RLock()
-	defer f.mutex.RUnlock()
-	return f.personaStateFlags
-}
-
-func (f *Friend) GameAppId() uint64 {
-	f.mutex.RLock()
-	defer f.mutex.RUnlock()
-	return f.gameAppId
-}
-
-// A thread-safe group list which contains references to its predecessor and successor.
-// It is mutable and will be changed by Social.
-type GroupsList struct {
-	mutex sync.RWMutex
-
-	first *Group
-	last  *Group
-
-	byId map[SteamId]*Group // fast lookup by ID
-}
-
-func (list *GroupsList) add(group *Group) {
-	list.mutex.Lock()
-	defer list.mutex.Unlock()
-
-	list.byId[group.steamId] = group
-
-	if list.first == nil {
-		list.first = group
-		list.last = group
-	} else {
-		group.prev = list.last
-		list.last.next = group
-		list.last = group
-	}
-}
-
-func (list *GroupsList) remove(id SteamId) {
-	list.mutex.Lock()
-	defer list.mutex.Unlock()
-
-	group := list.byId[id]
-	if group == nil {
-		return
-	}
-
-	if list.first == group {
-		list.first = nil
-	} else {
-		group.prev.next = group.next
-	}
-
-	if list.last == group {
-		list.last = nil
-	} else {
-		group.next.prev = group.prev
-	}
-}
-
-// Returns the first group in the group list or nil if the list is empty.
-func (list *GroupsList) First() *Group {
-	list.mutex.RLock()
-	defer list.mutex.RUnlock()
-	return list.first
-}
-
-// Returns the last group in the group list or nil if the list is empty.
-func (list *GroupsList) Last() *Group {
-	list.mutex.RLock()
-	defer list.mutex.RUnlock()
-	return list.last
-}
-
-// Returns the group by a SteamId or nil if there is no such group.
-func (list *GroupsList) ById(id SteamId) *Group {
-	list.mutex.RLock()
-	defer list.mutex.RUnlock()
-	return list.byId[id]
-}
-
-func (list *GroupsList) Count() int {
-	list.mutex.Lock()
-	defer list.mutex.Unlock()
-	return len(list.byId)
-}
-
-// A thread-safe group in a group list which contains references to its predecessor and successor.
-// It is mutable and will be changed by Social.
-type Group struct {
-	mutex sync.RWMutex
-
-	prev *Group
-	next *Group
-
-	steamId      SteamId
-	name         string
-	relationship EClanRelationship
-}
-
-// Returns the previous element in the group list or nil if this group is the first in the list.
-func (g *Group) Prev() *Group {
-	g.mutex.RLock()
-	defer g.mutex.RUnlock()
-	return g.prev
-}
-
-// Returns the next element in the group list or nil if this group is the last in the list.
-func (g *Group) Next() *Group {
-	g.mutex.RLock()
-	defer g.mutex.RUnlock()
-	return g.next
-}
-
-func (g *Group) SteamId() SteamId {
-	// the steam id of a group never changes, so we don't need to lock here
-	return g.steamId
-}
-
-func (g *Group) Name() string {
-	g.mutex.RLock()
-	defer g.mutex.RUnlock()
-	return g.name
-}
-
-func (g *Group) Relationship() EClanRelationship {
-	g.mutex.RLock()
-	defer g.mutex.RUnlock()
-	return g.relationship
 }
